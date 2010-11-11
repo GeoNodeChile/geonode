8ffef8f1eeedd014ae1a835fc1e9db313a6cbcc4
Fernando Leal <fdo_ls@yahoo.es>
2010-11-11 19:11:30 -0300

    Went back to the index file living in geonode.views to avoid breaking other pages

--- src/GeoNodePy/geonode/views.py
+++ src/GeoNodePy/geonode/views.py
@@ -7,9 +7,10 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 import json
+from geonode.maps.views import index
 
-def index(request): 
-    return render_to_response('index.html', RequestContext(request))
+#def index(request): 
+#    return render_to_response('index.html', RequestContext(request))
 
 def static(request, page):
     return render_to_response(page + '.html', RequestContext(request, {
